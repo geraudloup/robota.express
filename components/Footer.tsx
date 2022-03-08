527f2097862d8b03ddfbd86e2919b4f65c24c6a2
Antoine Garcia <antoine.garcia@ardian.com>
2022-03-08 20:48:37 +0100

    add footer with logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,6 @@ import styles from '../styles/Home.module.css'
 
 const Footer: NextPage = () => {
   return (
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -13,7 +12,7 @@ const Footer: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/logo.svg" alt="Robota Express Logo" width={144} height={32} />
           </span>
         </a>
       </footer>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { NextUIProvider } from '@nextui-org/react';
+import Footer from '../components/Footer';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <Component {...pageProps} />
+      <Footer />
     </NextUIProvider>
   );
 }
